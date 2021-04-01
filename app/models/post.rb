ca277b62b9b4ed9bd82f767c4acec6b5bba578ff
Aiden <aiden.marques94@gmail.com>
2021-03-31 21:53:36 -0400

    commit tree f27498c925cdc36dbe39a403ba3b92c7349ecd6e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length:{minimum: 100}
+validates :category, inclusion:{in:%w(Fiction Non-Fiction),
+message:""}
+
+
 end
